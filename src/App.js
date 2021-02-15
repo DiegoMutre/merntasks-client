9852202e41026b5d90c3cfbba51f42a805dd020b
DiegoMutre <tysonmutre10@gmail.com>
2021-02-15 06:13:12 -0500

    feat: Add BrowserRouter to App component with three routes.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Login from "./components/auth/Login";
+import NewAccount from "./components/auth/NewAccount";
+import Projects from "./components/projects/Projects";
+
 function App() {
-    return <h1>MERNTasks</h1>;
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={Login} />
+                <Route exact path="/new-account" component={NewAccount} />
+                <Route exact path="/projects" component={Projects} />
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
