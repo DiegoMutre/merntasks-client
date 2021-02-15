1727d3310c09a78ce9f7d72bab9415d35a82e183
DiegoMutre <tysonmutre10@gmail.com>
2021-02-15 16:46:28 -0500

    feat: Add a new type action.

--- src/types/index.js
+++ src/types/index.js
@@ -6,3 +6,4 @@ export const GET_CURRENT_PROJECT = "GET_CURRENT_PROJECT";
 export const DELETE_PROJECT = "DELETE_PROJECT";
 
 export const GET_TASKS_BY_ID = "GET_TASKS_BY_ID";
+export const ADD_TASK = "ADD_TASK";
