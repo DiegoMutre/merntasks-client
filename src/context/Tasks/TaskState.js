ac5e0a57cbe834915f228bcb020882f3484ce47d
DiegoMutre <tysonmutre10@gmail.com>
2021-02-15 16:03:51 -0500

    feat: Create TaskState.

---
+++ src/context/Tasks/TaskState.js
@@ -0,0 +1,15 @@
+import { useReducer } from "react";
+import TaskContext from "./TaskContext";
+import TaskReducer from "./TaskReducer";
+
+const TaskState = props => {
+    const initialState = {
+        tasks: [],
+    };
+
+    const [state, dispatch] = useReducer(TaskReducer, initialState);
+
+    return <TaskContext.Provider>{props.children}</TaskContext.Provider>;
+};
+
+export default TaskState;
