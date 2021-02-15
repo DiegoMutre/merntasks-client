edaa55f0534b2ff71b6ccced98c0c3e7a92ceac4
DiegoMutre <tysonmutre10@gmail.com>
2021-02-15 07:28:26 -0500

    feat: Create Project component.

---
+++ src/components/projects/Project.js
@@ -0,0 +1,9 @@
+const Project = ({ project }) => {
+    return (
+        <li>
+            <button className="btn btn-blank">{project.name}</button>
+        </li>
+    );
+};
+
+export default Project;
