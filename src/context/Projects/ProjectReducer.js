b87c1b335202b53569e98210f9d86403c0727115
DiegoMutre <tysonmutre10@gmail.com>
2021-02-15 12:06:37 -0500

    feat: Create action GET_PROJECTS in ProjectReducer

--- src/context/Projects/ProjectReducer.js
+++ src/context/Projects/ProjectReducer.js
@@ -1,9 +1,14 @@
-import { PROJECT_FORM } from "../../types";
+import { GET_PROJECTS, PROJECT_FORM } from "../../types";
 
 const projectReducer = (state, action) => {
     switch (action.type) {
         case PROJECT_FORM:
             return { ...state, showForm: !state.showForm };
+        case GET_PROJECTS:
+            return {
+                ...state,
+                projects: action.payload,
+            };
         default:
             return state;
     }
