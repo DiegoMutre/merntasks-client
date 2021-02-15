561212231ca47509965e3e60f3b61d402a9c8d52
DiegoMutre <tysonmutre10@gmail.com>
2021-02-15 08:38:50 -0500

    feat: Use ProjectState in App component.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from "./components/auth/Login";
 import NewAccount from "./components/auth/NewAccount";
 import Projects from "./components/projects/Projects";
+import ProjectState from "./context/Projects/ProjectState";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={Login} />
-                <Route exact path="/new-account" component={NewAccount} />
-                <Route exact path="/projects" component={Projects} />
-            </Switch>
-        </BrowserRouter>
+        <ProjectState>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={Login} />
+                    <Route exact path="/new-account" component={NewAccount} />
+                    <Route exact path="/projects" component={Projects} />
+                </Switch>
+            </BrowserRouter>
+        </ProjectState>
     );
 }
 
