e54ba8777b3c3402d70c7391e837b1ae015ad421
DiegoMutre <tysonmutre10@gmail.com>
2021-02-15 09:05:45 -0500

    feat: Use type in ProjectReducer.

--- src/context/Projects/ProjectReducer.js
+++ src/context/Projects/ProjectReducer.js
@@ -1,5 +1,9 @@
+import { PROJECT_FORM } from "../../types";
+
 const projectReducer = (state, action) => {
     switch (action.type) {
+        case PROJECT_FORM:
+            return { ...state, showForm: !state.showForm };
         default:
             return state;
     }
