dc70ef0382fc5b050eab69d4c7b664d1c791d84b
DiegoMutre <tysonmutre10@gmail.com>
2021-02-15 16:23:35 -0500

    feat: Use getTasksById function in Project component.

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -1,15 +1,19 @@
 import { useContext } from "react";
 import ProjectContext from "../../context/Projects/ProjectContext";
+import TaskContext from "../../context/Tasks/TaskContext";
 
 const Project = ({ project }) => {
     const { getCurrentProject } = useContext(ProjectContext);
+    const { getTasksById } = useContext(TaskContext);
+
+    const handleClick = () => {
+        getCurrentProject(project.id);
+        getTasksById(project.id);
+    };
 
     return (
         <li>
-            <button
-                className="btn btn-blank"
-                onClick={() => getCurrentProject(project.id)}
-            >
+            <button className="btn btn-blank" onClick={handleClick}>
                 {project.name}
             </button>
         </li>
