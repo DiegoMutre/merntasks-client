340d2c420a7d447d9220af54cee66c41e897ce85
DiegoMutre <tysonmutre10@gmail.com>
2021-02-15 17:54:17 -0500

    feat: Create function deleteTask in TaskState.

--- src/context/Tasks/TaskState.js
+++ src/context/Tasks/TaskState.js
@@ -1,23 +1,28 @@
 import { useReducer } from "react";
-import { ADD_TASK, GET_TASKS_BY_ID, SHOW_TASK_ERROR } from "../../types";
+import {
+    ADD_TASK,
+    DELETE_TASK,
+    GET_TASKS_BY_ID,
+    SHOW_TASK_ERROR,
+} from "../../types";
 import TaskContext from "./TaskContext";
 import TaskReducer from "./TaskReducer";
 
 const TaskState = props => {
     const initialState = {
         tasks: [
-            { name: "Choose platform", state: true, projectId: 3 },
-            { name: "Choose colors 3", state: false, projectId: 3 },
-            { name: "Choose Hosting 3", state: false, projectId: 3 },
-            { name: "Choose Typography", state: false, projectId: 3 },
-            { name: "Choose platform", state: true, projectId: 1 },
-            { name: "Choose colors 1", state: false, projectId: 1 },
-            { name: "Choose Hosting 1", state: false, projectId: 1 },
-            { name: "Choose Typography", state: false, projectId: 1 },
-            { name: "Choose platform", state: true, projectId: 2 },
-            { name: "Choose colors 2", state: false, projectId: 2 },
-            { name: "Choose Hosting 2", state: false, projectId: 2 },
-            { name: "Choose Typography", state: false, projectId: 2 },
+            { id: 0, name: "Choose platform", state: true, projectId: 3 },
+            { id: 1, name: "Choose colors 3", state: false, projectId: 3 },
+            { id: 2, name: "Choose Hosting 3", state: false, projectId: 3 },
+            { id: 3, name: "Choose Typography", state: false, projectId: 3 },
+            { id: 4, name: "Choose platform", state: true, projectId: 1 },
+            { id: 5, name: "Choose colors 1", state: false, projectId: 1 },
+            { id: 6, name: "Choose Hosting 1", state: false, projectId: 1 },
+            { id: 7, name: "Choose Typography", state: false, projectId: 1 },
+            { id: 8, name: "Choose platform", state: true, projectId: 2 },
+            { id: 9, name: "Choose colors 2", state: false, projectId: 2 },
+            { id: 10, name: "Choose Hosting 2", state: false, projectId: 2 },
+            { id: 11, name: "Choose Typography", state: false, projectId: 2 },
         ],
         projectTasks: null,
         taskHasError: false,
@@ -45,6 +50,13 @@ const TaskState = props => {
         });
     };
 
+    const deleteTask = id => {
+        dispatch({
+            type: DELETE_TASK,
+            payload: id,
+        });
+    };
+
     return (
         <TaskContext.Provider
             value={{
@@ -54,6 +66,7 @@ const TaskState = props => {
                 getTasksById,
                 addTask,
                 showTaskError,
+                deleteTask,
             }}
         >
             {props.children}
