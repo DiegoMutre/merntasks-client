e5bab5e726d1dc1a9914b9ba022923ccde71d947
DiegoMutre <tysonmutre10@gmail.com>
2021-02-15 16:13:08 -0500

    feat: Create fake tasks array in TaskState.

--- src/context/Tasks/TaskState.js
+++ src/context/Tasks/TaskState.js
@@ -4,12 +4,33 @@ import TaskReducer from "./TaskReducer";
 
 const TaskState = props => {
     const initialState = {
-        tasks: [],
+        tasks: [
+            { name: "Choose platform", state: true, projectId: 3 },
+            { name: "Choose colors 3", state: false, projectId: 3 },
+            { name: "Choose Hosting 3", state: false, projectId: 3 },
+            { name: "Choose Typography", state: false, projectId: 3 },
+            { name: "Choose platform", state: true, projectId: 1 },
+            { name: "Choose colors 1", state: false, projectId: 1 },
+            { name: "Choose Hosting 1", state: false, projectId: 1 },
+            { name: "Choose Typography", state: false, projectId: 1 },
+            { name: "Choose platform", state: true, projectId: 2 },
+            { name: "Choose colors 2", state: false, projectId: 2 },
+            { name: "Choose Hosting 2", state: false, projectId: 2 },
+            { name: "Choose Typography", state: false, projectId: 2 },
+        ],
     };
 
     const [state, dispatch] = useReducer(TaskReducer, initialState);
 
-    return <TaskContext.Provider>{props.children}</TaskContext.Provider>;
+    return (
+        <TaskContext.Provider
+            value={{
+                tasks: state.tasks,
+            }}
+        >
+            {props.children}
+        </TaskContext.Provider>
+    );
 };
 
 export default TaskState;
