82dfdc5f07fa2744480c8f5a8672dc2989fa9783
DiegoMutre <tysonmutre10@gmail.com>
2021-02-15 15:08:47 -0500

    feat: Use getCurrentProject in Project component.

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -1,7 +1,17 @@
+import { useContext } from "react";
+import ProjectContext from "../../context/Projects/ProjectContext";
+
 const Project = ({ project }) => {
+    const { getCurrentProject } = useContext(ProjectContext);
+
     return (
         <li>
-            <button className="btn btn-blank">{project.name}</button>
+            <button
+                className="btn btn-blank"
+                onClick={() => getCurrentProject(project.id)}
+            >
+                {project.name}
+            </button>
         </li>
     );
 };
