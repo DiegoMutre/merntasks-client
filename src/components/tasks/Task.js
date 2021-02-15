9218b83fbebe4c24de56e435b0616e7014040066
DiegoMutre <tysonmutre10@gmail.com>
2021-02-15 08:19:50 -0500

    feat: Finish Task component structure.

--- src/components/tasks/Task.js
+++ src/components/tasks/Task.js
@@ -1,5 +1,20 @@
 const Task = ({ task }) => {
-    return <li>From Task</li>;
+    return (
+        <li className="tarea sombra">
+            <p>{task.name}</p>
+            <div className="estado">
+                {task.state ? (
+                    <button className="completo">Complete</button>
+                ) : (
+                    <button className="incompleto">Incomplete</button>
+                )}
+            </div>
+            <div className="acciones">
+                <button className="btn btn-primario">Edit</button>
+                <button className="btn btn-secundario">Delete</button>
+            </div>
+        </li>
+    );
 };
 
 export default Task;
