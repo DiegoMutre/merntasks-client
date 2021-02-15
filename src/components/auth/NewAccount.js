5b5585f59a426cb50d9072b76fa3c2383300707d
DiegoMutre <tysonmutre10@gmail.com>
2021-02-15 06:46:19 -0500

    feat: Create NewAccount component structure and create events.

--- src/components/auth/NewAccount.js
+++ src/components/auth/NewAccount.js
@@ -1,5 +1,95 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const NewAccount = () => {
-    return <h1>From NewAccount</h1>;
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        password: "",
+        confirm: "",
+    });
+
+    const { name, email, password, confirm } = user;
+
+    const handleChange = e => {
+        setUser({ ...user, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        // TODO: Validate
+
+        // TODO: Minimum password 6 characters
+
+        // TODO: The 2 password are the same
+
+        // TODO: Pass to action
+    };
+
+    return (
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Get an Account</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="campo-form">
+                        <label htmlFor="name">Name</label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            placeholder="Your name"
+                            onChange={handleChange}
+                            value={name}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Your email"
+                            onChange={handleChange}
+                            value={email}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Your password"
+                            onChange={handleChange}
+                            value={password}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="confirm">Confirm Password</label>
+                        <input
+                            type="password"
+                            id="confirm"
+                            name="confirm"
+                            placeholder="Repeat your password"
+                            onChange={handleChange}
+                            value={confirm}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <input
+                            type="submit"
+                            className="btn btn-primario btn-block"
+                            value="Sign up"
+                        />
+                    </div>
+                </form>
+                <Link to={"/"} className="enlace-cuenta">
+                    Return to Log in
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default NewAccount;
