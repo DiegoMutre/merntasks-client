f8c8a6364977ff56ea47008e3ebb6d50c6317566
DiegoMutre <tysonmutre10@gmail.com>
2021-02-15 09:06:17 -0500

    feat: Create function that executes the dispatch.

--- src/context/Projects/ProjectState.js
+++ src/context/Projects/ProjectState.js
@@ -1,4 +1,5 @@
 import { useReducer } from "react";
+import { PROJECT_FORM } from "../../types";
 import ProjectContext from "./ProjectContext";
 import projectReducer from "./ProjectReducer";
 
@@ -10,10 +11,17 @@ const ProjectState = props => {
     // Dispatch to execute the actions
     const [state, dispatch] = useReducer(projectReducer, initialState);
 
+    const toggleForm = () => {
+        dispatch({
+            type: PROJECT_FORM,
+        });
+    };
+
     return (
         <ProjectContext.Provider
             value={{
                 showForm: state.showForm,
+                toggleForm,
             }}
         >
             {props.children}
