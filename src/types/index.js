e604b3dcca7edc0d70b0f8b985197c9a7317ec66
DiegoMutre <tysonmutre10@gmail.com>
2021-02-15 17:53:06 -0500

    feat: Create new action type.

--- src/types/index.js
+++ src/types/index.js
@@ -8,3 +8,4 @@ export const DELETE_PROJECT = "DELETE_PROJECT";
 export const GET_TASKS_BY_ID = "GET_TASKS_BY_ID";
 export const ADD_TASK = "ADD_TASK";
 export const SHOW_TASK_ERROR = "SHOW_TASK_ERROR";
+export const DELETE_TASK = "DELETE_TASK";
