6a3321c0a931c79df222ef8f6e0fcf2d1385e0e1
DiegoMutre <tysonmutre10@gmail.com>
2021-02-15 06:23:02 -0500

    feat: Create Login component structure.

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,5 +1,42 @@
 const Login = () => {
-    return <h1>From Login</h1>;
+    const handleChange = () => {};
+
+    return (
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Log in</h1>
+                <form>
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Your email"
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Your password"
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <input
+                            type="submit"
+                            className="btn btn-primario btn-block"
+                            value="Log in"
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
