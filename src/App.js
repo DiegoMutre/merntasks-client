94882e3a8b65ae62817cb7a35e59c5df117ebecc
DiegoMutre <tysonmutre10@gmail.com>
2021-02-15 16:04:54 -0500

    feat: Use TaskState provider in App component.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import Login from "./components/auth/Login";
 import NewAccount from "./components/auth/NewAccount";
 import Projects from "./components/projects/Projects";
 import ProjectState from "./context/Projects/ProjectState";
+import TaskState from "./context/Tasks/TaskState";
 
 function App() {
     return (
         <ProjectState>
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/" component={Login} />
-                    <Route exact path="/new-account" component={NewAccount} />
-                    <Route exact path="/projects" component={Projects} />
-                </Switch>
-            </BrowserRouter>
+            <TaskState>
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path="/" component={Login} />
+                        <Route
+                            exact
+                            path="/new-account"
+                            component={NewAccount}
+                        />
+                        <Route exact path="/projects" component={Projects} />
+                    </Switch>
+                </BrowserRouter>
+            </TaskState>
         </ProjectState>
     );
 }
