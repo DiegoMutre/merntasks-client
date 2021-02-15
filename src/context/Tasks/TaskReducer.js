fb558b90020e94ba66b706e75b37e22ae69d27e1
DiegoMutre <tysonmutre10@gmail.com>
2021-02-15 16:38:52 -0500

    feat: Create GET_TASKS_BY_ID action in TaskReducer

--- src/context/Tasks/TaskReducer.js
+++ src/context/Tasks/TaskReducer.js
@@ -1,5 +1,15 @@
+import { GET_TASKS_BY_ID } from "../../types";
+
 const TaskReducer = (state, action) => {
     switch (action.type) {
+        case GET_TASKS_BY_ID:
+            return {
+                ...state,
+                projectTasks: state.tasks.filter(
+                    task => task.projectId === action.payload
+                ),
+            };
+
         default:
             return state;
     }
