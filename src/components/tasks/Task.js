9b88d00fdfa7f06ebf4d1a6a38273231d2d7de27
DiegoMutre <tysonmutre10@gmail.com>
2021-02-15 17:55:24 -0500

    feat: Use deleteTask function in Task component.

--- src/components/tasks/Task.js
+++ src/components/tasks/Task.js
@@ -1,4 +1,16 @@
+import { useContext } from "react";
+import ProjectContext from "../../context/Projects/ProjectContext";
+import TaskContext from "../../context/Tasks/TaskContext";
+
 const Task = ({ task }) => {
+    const { deleteTask, getTasksById } = useContext(TaskContext);
+    const { project } = useContext(ProjectContext);
+
+    const handleDelete = () => {
+        deleteTask(task.id);
+        getTasksById(project[0].id);
+    };
+
     return (
         <li className="tarea sombra">
             <p>{task.name}</p>
@@ -11,7 +23,9 @@ const Task = ({ task }) => {
             </div>
             <div className="acciones">
                 <button className="btn btn-primario">Edit</button>
-                <button className="btn btn-secundario">Delete</button>
+                <button className="btn btn-secundario" onClick={handleDelete}>
+                    Delete
+                </button>
             </div>
         </li>
     );
