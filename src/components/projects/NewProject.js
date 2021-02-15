b37a903f24301e348f239be55abbfd202832c6cf
DiegoMutre <tysonmutre10@gmail.com>
2021-02-15 08:47:12 -0500

    feat: Use useContext hook in NewProject component.

--- src/components/projects/NewProject.js
+++ src/components/projects/NewProject.js
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import ProjectContext from "../../context/Projects/ProjectContext";
 
 const NewProject = () => {
+    const { showForm } = useContext(ProjectContext);
+
     const [project, setProject] = useState({ name: "" });
 
     const handleChange = e => {
@@ -22,21 +25,26 @@ const NewProject = () => {
             <button className="btn btn-block btn-primario" type="button">
                 New Project
             </button>
-            <form className="formulario-nuevo-proyecto" onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    className="input-text"
-                    placeholder="Project Name"
-                    name="name"
-                    value={project.name}
-                    onChange={handleChange}
-                />
-                <input
-                    type="submit"
-                    value="Add Project"
-                    className="btn btn-primario btn-block"
-                />
-            </form>
+            {showForm && (
+                <form
+                    className="formulario-nuevo-proyecto"
+                    onSubmit={handleSubmit}
+                >
+                    <input
+                        type="text"
+                        className="input-text"
+                        placeholder="Project Name"
+                        name="name"
+                        value={project.name}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="submit"
+                        value="Add Project"
+                        className="btn btn-primario btn-block"
+                    />
+                </form>
+            )}
         </>
     );
 };
