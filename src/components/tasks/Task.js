8aaa6f4316821e3c9fd9b1f817596c47c66eb216
DiegoMutre <tysonmutre10@gmail.com>
2021-02-15 18:38:57 -0500

    feat: Use changeTaskState function in Task component.

--- src/components/tasks/Task.js
+++ src/components/tasks/Task.js
@@ -3,7 +3,9 @@ import ProjectContext from "../../context/Projects/ProjectContext";
 import TaskContext from "../../context/Tasks/TaskContext";
 
 const Task = ({ task }) => {
-    const { deleteTask, getTasksById } = useContext(TaskContext);
+    const { deleteTask, getTasksById, changeTaskState } = useContext(
+        TaskContext
+    );
     const { project } = useContext(ProjectContext);
 
     const handleDelete = () => {
@@ -11,14 +13,27 @@ const Task = ({ task }) => {
         getTasksById(project[0].id);
     };
 
+    const handleClick = () => {
+        if (task.state) {
+            task.state = false;
+        } else {
+            task.state = true;
+        }
+        changeTaskState(task);
+    };
+
     return (
         <li className="tarea sombra">
             <p>{task.name}</p>
             <div className="estado">
                 {task.state ? (
-                    <button className="completo">Complete</button>
+                    <button className="completo" onClick={handleClick}>
+                        Complete
+                    </button>
                 ) : (
-                    <button className="incompleto">Incomplete</button>
+                    <button className="incompleto" onClick={handleClick}>
+                        Incomplete
+                    </button>
                 )}
             </div>
             <div className="acciones">
