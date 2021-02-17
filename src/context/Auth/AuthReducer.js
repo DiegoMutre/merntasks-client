b07001e8940c9082ca05d5305cdea9eff01c757e
DiegoMutre <tysonmutre10@gmail.com>
2021-02-17 07:02:01 -0500

    feat: Create action types in AuthReducer.

--- src/context/Auth/AuthReducer.js
+++ src/context/Auth/AuthReducer.js
@@ -1,5 +1,20 @@
+import { REGISTRATION_ERROR, REGISTRATION_SUCCESSFUL } from "../../types";
+
 const AuthReducer = (state, action) => {
     switch (action.type) {
+        case REGISTRATION_SUCCESSFUL:
+            localStorage.setItem("token", action.payload.token);
+            return {
+                ...state,
+                authenticated: true,
+                msg: null,
+            };
+        case REGISTRATION_ERROR:
+            return {
+                ...state,
+                token: null,
+                msg: action.payload,
+            };
         default:
             return state;
     }
