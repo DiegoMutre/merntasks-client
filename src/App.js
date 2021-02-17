8e62ee1ee2c4d020559caeab3cdb6508c7e308d1
DiegoMutre <tysonmutre10@gmail.com>
2021-02-17 05:50:18 -0500

    feat: Use AlertState in App component.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from "./components/auth/Login";
 import NewAccount from "./components/auth/NewAccount";
 import Projects from "./components/projects/Projects";
+import AlertState from "./context/Alerts/AlertState";
 import ProjectState from "./context/Projects/ProjectState";
 import TaskState from "./context/Tasks/TaskState";
 
@@ -9,17 +10,23 @@ function App() {
     return (
         <ProjectState>
             <TaskState>
-                <BrowserRouter>
-                    <Switch>
-                        <Route exact path="/" component={Login} />
-                        <Route
-                            exact
-                            path="/new-account"
-                            component={NewAccount}
-                        />
-                        <Route exact path="/projects" component={Projects} />
-                    </Switch>
-                </BrowserRouter>
+                <AlertState>
+                    <BrowserRouter>
+                        <Switch>
+                            <Route exact path="/" component={Login} />
+                            <Route
+                                exact
+                                path="/new-account"
+                                component={NewAccount}
+                            />
+                            <Route
+                                exact
+                                path="/projects"
+                                component={Projects}
+                            />
+                        </Switch>
+                    </BrowserRouter>
+                </AlertState>
             </TaskState>
         </ProjectState>
     );
