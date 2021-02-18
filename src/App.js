8c42e3f2f6220f3a13f91545ba2c01ba03b92cf8
DiegoMutre <tysonmutre10@gmail.com>
2021-02-18 06:16:21 -0500

    feat: Set token in App component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { useEffect } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from "./components/auth/Login";
 import NewAccount from "./components/auth/NewAccount";
 import Projects from "./components/projects/Projects";
+import setHeaderToken from "./config/token";
 import AlertState from "./context/Alerts/AlertState";
 import AuthState from "./context/Auth/AuthState";
 import ProjectState from "./context/Projects/ProjectState";
 import TaskState from "./context/Tasks/TaskState";
 
 function App() {
+    const token = localStorage.getItem("token");
+    if (token) {
+        setHeaderToken(token);
+    }
+
     return (
         <ProjectState>
             <TaskState>
