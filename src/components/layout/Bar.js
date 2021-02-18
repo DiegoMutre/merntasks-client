dd409a03b674aafc3ce86a9e12078242ed898486
DiegoMutre <tysonmutre10@gmail.com>
2021-02-18 06:29:11 -0500

    feat: Show username in Bar component.

--- src/components/layout/Bar.js
+++ src/components/layout/Bar.js
@@ -1,9 +1,20 @@
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../../context/Auth/AuthContext";
+
 const Bar = () => {
+    const { user, getUserAuthenticated } = useContext(AuthContext);
+
+    useEffect(() => {
+        getUserAuthenticated();
+    }, []);
+
     return (
         <header className="app-header">
-            <p className="nombre-usuario">
-                Hi <span>Diego Mutre</span>
-            </p>
+            {user && (
+                <p className="nombre-usuario">
+                    Hi <span>{user.name}</span>
+                </p>
+            )}
             <nav className="nav-principal">
                 <a href="#!">Sign off</a>
             </nav>
