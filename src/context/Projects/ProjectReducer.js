47293b14bb4e1b3fd4e1388004cf8746a57d87e7
DiegoMutre <tysonmutre10@gmail.com>
2021-02-18 17:22:23 -0500

    feat: Change 'id' for '_id' in ProjectReducer.

--- src/context/Projects/ProjectReducer.js
+++ src/context/Projects/ProjectReducer.js
@@ -32,14 +32,14 @@ const projectReducer = (state, action) => {
             return {
                 ...state,
                 project: state.projects.filter(
-                    project => project.id === action.payload
+                    project => project._id === action.payload
                 ),
             };
         case DELETE_PROJECT:
             return {
                 ...state,
                 projects: state.projects.filter(
-                    project => project.id !== action.payload
+                    project => project._id !== action.payload
                 ),
                 project: null,
             };
